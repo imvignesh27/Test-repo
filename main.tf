3cc3fc3cdac6e93e374a06a309c4873a6b838114
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-19 00:30:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -51,14 +51,4 @@ resource "aws_s3_bucket_versioning" "versioning" {
     status = "Enabled"
   }
 }
-
-# Enable default encryption (AES256)
-resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
-  bucket = aws_s3_bucket.cis_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
 }
