4f392313bf54ed247eb0d7b5b2de5d17c99ab338
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-18 23:38:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_s3_bucket" "cis_bucket" {
-  bucket = "cis-compliant-demo-bucket"
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.cis_bucket.id
   acl    = "private"
 }
 
