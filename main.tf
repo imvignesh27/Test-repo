07ee7e91027fb6eac4e8b77237107f3207ba50f2
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-19 03:04:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,21 +27,8 @@ resource "aws_config_config_rule" "s3_version_lifecycle" {
 
 resource "aws_config_remediation_configuration" "s3_remediate" {
   config_rule_name = aws_config_config_rule.s3_version_lifecycle.name
-  target_id        = "AWS-UpdateS3BucketVersioning"   # Built-in SSM Document for remediation
+  target_id        = "AWS-UpdateS3BucketVersioning"
   target_type      = "SSM_DOCUMENT"
-  
-  automatic = true
 
-  parameters = {
-    BucketName = {
-      static_value = {
-        values = ["${aws_s3_bucket.example_bucket.bucket}"]
-      }
-    }
-    VersioningConfiguration = {
-      static_value = {
-        values = ["Enabled"]
-      }
-    }
-  }
+  automatic = true
 }
