708872505f214f929243567bb7300ed884b6fb36
Vignesh <79822800+imvignesh27@users.noreply.github.com>
2025-09-21 22:22:28 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "ap-south-1"
 }
 
 variable "iam_user_name" {
   description = "IAM User name to create (CIS)"
   type        = string
-  default     = cis-user-1
 }
 
 variable "non_cis_user_name" {
   description = "IAM User name to create (Non-CIS)"
   type        = string
-  default     = non-cis-user-1
 }
 
 variable "s3_bucket_name" {
